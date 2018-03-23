12297f7a58086894ca8fae6407cfa7e3660139d7
raphaelattias <raphael.attias@outlook.com>
2018-03-23 16:49:19 +0100

    Create pendule.cpp

---
+++ pendule.cpp
@@ -0,0 +1,18 @@
+//
+//  pendule.cpp
+//  PROJET
+//
+//  Created by Raphael Attias on 23/03/2018.
+//  Copyright © 2018 Raphael Attias. All rights reserved.
+//
+
+#include "pendule.h"
+#include "oscillateur.h"
+#include <cmath>
+
+class Vecteur Pendule::evolutionPendule(){
+    class Vecteur sortie((-9.81/longueur)*vecteur_etat.get_value(1), 0, 0);
+        return sortie;
+};
+
+
