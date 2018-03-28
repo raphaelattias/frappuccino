16bcf8682baa2b4dbe6e94c88c1cf7764becf97a
raphaelattias <raphael.attias@outlook.com>
2018-03-28 17:43:34 +0200

    Update testintegrateur.cpp

--- testintegrateur.cpp
+++ testintegrateur.cpp
@@ -9,39 +9,64 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
-#include "Oscillateur.h"
+#include "oscillateur.h"
 #include "vecteur.h"
 #include "pendule.h"
+#include <iomanip>
 
 using namespace std;
 
+
+/*
+ 
+ 
+ 
+ COMMENT REDEFINIR LA FONCTION D'EVOLUTION DANS CHAQUE SOUS CLASSE D'OSCILLATEUR
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
+
 class Integrateur{
 public:
-    Integrateur(double pas_de_temps=0.1): pas_de_temps(pas_de_temps){};
+    Integrateur(double pas_de_temps=0.01): pas_de_temps(pas_de_temps){};
     void afficherEvolution(Oscillateur const& oscillateur) const{
-        cout << "Vecteur d'état :" << oscillateur.get_vitesse();
-        cout << endl << "Vecteur de vitesse :" << oscillateur.get_etat();
+        cout << "Vecteur de position :" << setprecision(10) << oscillateur.get_position();
+        cout << endl << "Vecteur de vitesse :" << setprecision(10) << oscillateur.get_vitesse();
         cout << endl;
     }
 protected:
     double pas_de_temps;
 };
 
+
 class IntegrateurEulerCromer: public Integrateur {
 public:
     IntegrateurEulerCromer(double pas_de_temps):Integrateur(pas_de_temps){};
     void integrerEC(Oscillateur &O1){
-        O1.set_vecteurs(O1.get_vitesse() + O1.evolution(pas_de_temps), O1.get_etat() + pas_de_temps*O1.get_vitesse());
+        O1.set_vecteurs(O1.get_position(), O1.get_vitesse() + pas_de_temps*O1.evolution());
+        O1.set_vecteurs(O1.get_position() + pas_de_temps*O1.get_vitesse(), O1.get_vitesse());
     }
 };
 
 
 int main(){
-    Vecteur v0;
-    Vecteur v1(3.14/2, 0, 0);
-    Vecteur v2(0,0,0);
-    Pendule pendule;
-    cout << v1 << endl;
+    Pendule O1({0,1},{1,2}, 0.127);
+    IntegrateurEulerCromer I1(0.01);
+    
+        cout << O1.evolution();
+    
+    O1.afficher_evolution(); cout <<endl;
+    for(int i=0; i <= 1000; i++){
+    I1.integrerEC(O1);
+    O1.afficher_evolution(); cout << endl;
+
+
+    }
     /*
     O1.set_vecteurs(v1, v2);
     IntegrateurEulerCromer I1(0.00001);
