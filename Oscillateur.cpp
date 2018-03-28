7d5bef0830b5fe6561c8f23e24d51d9fc64339f3
raphaelattias <raphael.attias@outlook.com>
2018-03-28 17:42:25 +0200

    Update Oscillateur.cpp

--- Oscillateur.cpp
+++ Oscillateur.cpp
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Pascal Epple. All rights reserved.
 //
 
-#include "Oscillateur.h"
+#include "oscillateur.h"
 #include "vecteur.h"
 
 #include <iostream>
@@ -28,7 +28,7 @@ using namespace std;
  *
  */
 
-Vecteur vecteur_etat; //degrés de liberté, dimensions, paramètres ?
+Vecteur position; //degrés de liberté, dimensions, paramètres ?
 Vecteur vitesse;
 double masse;
 double longueur;
@@ -46,22 +46,21 @@ double longueur;
  *
  */
 
-Oscillateur::Oscillateur(Vecteur vecteur_etat, Vecteur vitesse, double masse, double longueur):vecteur_etat(vecteur_etat), vitesse(vitesse), masse(masse), longueur(longueur){};
+Oscillateur::Oscillateur(Vecteur vecteur_etat, Vecteur vitesse, double masse, double longueur):position(vecteur_etat), vitesse(vitesse), masse(masse), longueur(longueur){};
 
-Vecteur Oscillateur::evolution(double t) const {
-    Vecteur sortie;
-    sortie = t * vitesse;
+Vecteur Oscillateur::evolution() const {
+    Vecteur sortie({0, -9.81});
     return sortie;
 }
 
 void Oscillateur::afficher_evolution() const {
-    cout << "Vecteur d'état :"; vecteur_etat.affiche();
+    cout << "Vecteur d'état :"; position.affiche();
     cout << endl << "Vecteur de vitesse :"; vitesse.affiche();
     cout << endl;
 }
 
-void Oscillateur::set_vecteurs(Vecteur vect_etat, Vecteur vect_vitesse) {
-    vecteur_etat = vect_etat;
+void Oscillateur::set_vecteurs(Vecteur vect_position, Vecteur vect_vitesse) {
+    position = vect_position;
     vitesse = vect_vitesse;
 }
 
@@ -69,6 +68,6 @@ Vecteur Oscillateur::get_vitesse() const {
     return vitesse;
 }
 
-Vecteur Oscillateur::get_etat() const {
-    return vecteur_etat;
+Vecteur Oscillateur::get_position() const {
+    return position;
 }
