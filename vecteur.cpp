aa0ae244b0fe8c28e13c467db229f5a89defcae7
raphaelattias <raphael.attias@outlook.com>
2018-05-23 14:35:01 +0200

    Add files via upload

--- vecteur.cpp
+++ vecteur.cpp
@@ -7,22 +7,9 @@
 
 using namespace std;
 
-/*
- *
- *
- *
- *
- //////////////////CONSTRUCTEURS
- *
- *
- *
- *
- */
+// Constructeurs :
 
-//ENLEVER LA DIMENSION//
-
-Vecteur::Vecteur(size_t dimension)
-    { // Only in the declaration but not in the definition
+Vecteur::Vecteur(size_t dimension){
     for(size_t i=0; i < dimension; i++){
         augmente(0);
     }
@@ -34,33 +21,18 @@ Vecteur::Vecteur(double x, double y, double z) {
     augmente(z);
 }
 
-Vecteur::Vecteur(initializer_list<double> init_list) {
+Vecteur::Vecteur(initializer_list<double> const& init_list) {
     vecteur = vector<double>(init_list.begin(), init_list.end());
 }
 
-Vecteur::Vecteur(Vecteur const& V2): vecteur(V2.vecteur){}
+// Méthodes :
 
-/*
-*
-*
-*
-*
-//////////////////METHODES
-*
-*
-*
-*
-*/
-
-void Vecteur::augmente(double valeur_augmentee){
+void Vecteur::augmente(double const& valeur_augmentee){
     vecteur.push_back(valeur_augmentee);
 }
 
-
-
-void Vecteur::set_coord(size_t rang, double nouvelle_valeur){ //si size t est faux que faire
+void Vecteur::set_coord(size_t const& rang, double const&  nouvelle_valeur){
     try {
-        // J'ai utilisé la bibliothèque stdexcept qui gère mieux les exceptions et les erreurs, je trouve ça bg
         if(rang > vecteur.size()){
             throw runtime_error("Erreur: le rang entré est incorrect, plus grand/petit que la dimension du vecteur.");
         } else if (rang <= 0){
@@ -72,6 +44,7 @@ void Vecteur::set_coord(size_t rang, double nouvelle_valeur){ //si size t est fa
         cerr << ex.what() << endl;
     }
 }
+
 ostream& Vecteur::affiche(ostream& sortie) const {
     for(auto el: vecteur){
         sortie << el << " ";
@@ -80,19 +53,7 @@ ostream& Vecteur::affiche(ostream& sortie) const {
     return sortie;
 }
 
-
-
-double Vecteur::get_value(size_t i) const {
-    return vecteur[i-1];
-}
-
-size_t Vecteur::get_dimension() const {
-    return vecteur.size();
-}
-
-
-
-bool Vecteur::compare(Vecteur vecteur2) const {
+bool Vecteur::compare(Vecteur const& vecteur2) const {
     for (size_t i=0; i < vecteur.size(); i++) {
         if (vecteur2.vecteur[i] != vecteur[i]) {
             return false;
@@ -101,14 +62,13 @@ bool Vecteur::compare(Vecteur vecteur2) const {
     return true;
 }
 
-Vecteur Vecteur::addition(Vecteur vecteur2) const {
+Vecteur Vecteur::addition(Vecteur const& vecteur2) const {
     // On souhaite déterminer la plus grande dimension des deux vecteurs afin de pouvoir manipuler deux vecteurs de tailles différentes.
     // Aussi communément appelé dans la Communauté mathématiques "Plongement naturel"
     Vecteur vecteur_sortie;
     Vecteur va; Vecteur vb(vecteur2);
     va.vecteur = vecteur;
     va.equilibrage_vecteurs(vb); vb.equilibrage_vecteurs(va); //car on ne sait pas lequel des deux a la plus grande dimension
-    
     
     for(size_t i=0; i < va.vecteur.size(); i++){
         vecteur_sortie.vecteur.push_back(va.vecteur[i]+vb.vecteur[i]);
@@ -117,8 +77,8 @@ Vecteur Vecteur::addition(Vecteur vecteur2) const {
     return vecteur_sortie;
 }
 
-Vecteur Vecteur::soustraction(Vecteur vecteur2) const{
-
+Vecteur Vecteur::soustraction(Vecteur const& vecteur2) const{
+    
     Vecteur vecteur_sortie;
     
     Vecteur va; Vecteur vb(vecteur2);
@@ -150,8 +110,7 @@ Vecteur Vecteur::mult(double scalaire) const{
     return vecteur_sortie;
 }
 
-Vecteur Vecteur::prod_vect(Vecteur vecteur2) const{
-    // Prevoir le catch dans le cas où les vecteurs sont de dimension différente de 3 (produit vect pas défini)
+Vecteur Vecteur::prod_vect(Vecteur const& vecteur2) const{
     Vecteur vecteur_sortie;
     try {
         if(vecteur.size() != 3 and vecteur2.vecteur.size() != 3) {
@@ -170,8 +129,7 @@ Vecteur Vecteur::prod_vect(Vecteur vecteur2) const{
     return vecteur_sortie;
 }
 
-
-double Vecteur::prod_scalaire(Vecteur vecteur2) const{
+double Vecteur::prod_scalaire(Vecteur const& vecteur2) const{
     double resultat(0.0);
     for(size_t i = 0; i < min(vecteur.size(),vecteur2.vecteur.size()); i++){
         resultat += vecteur[i]*vecteur2.vecteur[i];
@@ -192,6 +150,10 @@ double Vecteur::norme() const{
     return sqrt(norme2());
 }
 
+bool Vecteur::test_unitaire() const {
+    return (norme() == 1);
+}
+
 void Vecteur::equilibrage_vecteurs (Vecteur &vecteur2){
     if (vecteur2.vecteur.size() < vecteur.size()) {
         for (size_t i(vecteur2.vecteur.size()+1); i <= vecteur.size(); i++) {
@@ -201,50 +163,49 @@ void Vecteur::equilibrage_vecteurs (Vecteur &vecteur2){
     
 }
 
+// Méthodes accesseurs :
+
+double Vecteur::get_value(size_t const& i) const {
+    return vecteur[i-1];
+}
+
+size_t Vecteur::get_dimension() const {
+    return vecteur.size();
+}
+
 void Vecteur::get_vecteur(Vecteur &vecteur2) {
     vecteur2.vecteur = vecteur;
 }
 
-
-bool Vecteur::test_unitaire() const {
-    return (norme() == 1);
-}
-
-/*
- *
- *
- *
- *
- //////////////////OPERATEURS
- *
- *
- *
- *
- */
+// Méthodes pour opérateurs internes
 
 bool Vecteur::operator==(Vecteur const& v2 ){
     return compare(v2);
 }
 
-const Vecteur operator+(Vecteur vect1, Vecteur const& vect2){
-    return vect1 += vect2;
+bool Vecteur::operator!=(Vecteur const& v2 ){
+    return !(*this==v2);
 }
 
-Vecteur& Vecteur::operator+=(Vecteur const& vect2){
-    Vecteur sortie;
-    sortie = addition(vect2);
-    *this = sortie;
+Vecteur& Vecteur::operator^=(Vecteur const& vect2){
+    *this = prod_vect(vect2);
     return *this;
-}
-
-const Vecteur operator-(Vecteur vect1, Vecteur const& vect2){
-    return vect1 -= vect2;
 }
 
 const Vecteur Vecteur::operator-(){
     Vecteur sortie;
     sortie = oppose();
     *this = sortie;
+    return *this;
+}
+
+Vecteur& Vecteur::operator*=(const double& i){
+    *this = mult(i);
+    return *this;
+}
+
+Vecteur& Vecteur::operator+=(Vecteur const& vect2){
+    *this = addition(vect2);
     return *this;
 }
 
@@ -255,50 +216,35 @@ Vecteur& Vecteur::operator-=(Vecteur const& vect2){
     return *this;
 }
 
-Vecteur& Vecteur::operator*=(const double& i){
-    *this = mult(i);
-    return *this;
-}
+// Méthodes pour opérateurs externes :
 
 double operator*(Vecteur v1, Vecteur const& v2){
     return v1.prod_scalaire(v2);
 }
 
+const Vecteur operator^(Vecteur vect1, Vecteur const& vect2){
+    return vect1 ^= vect2;
+}
+
+const Vecteur operator+(Vecteur vect1, Vecteur const& vect2){
+    return vect1 += vect2;
+}
+
+
+const Vecteur operator-(Vecteur vect1, Vecteur const& vect2){
+    return vect1 -= vect2;
+}
+
+
 const Vecteur operator*(double const& i, Vecteur vect1){
     return vect1 *= i;
 }
 
-//PROBLEME LORSQUE JE VEUX PASSER LE PREMIER ARGUMENT EN CONST&
-//PAREIL POUR AU-DESSUS
-
-const Vecteur operator*(Vecteur vect1, double i){
+const Vecteur operator*(Vecteur vect1, double  const& i){
     return vect1 *= i;
-}
-
-Vecteur& Vecteur::operator^=(Vecteur vect2){
-    *this = prod_vect(vect2);
-    return *this;
-}
-
-Vecteur operator^(Vecteur vect1, Vecteur vect2){
-    vect1 ^= vect2;
-    return vect1;
 }
 
 ostream& operator<<(ostream& sortie, Vecteur const& vect){
     return vect.affiche(sortie);
-};
+}
 
-/*
- *
- *
- *
- *
- //////////////////ATTRIBUTS
- *
- *
- *
- *
- */
-
-vector<double> vecteur;
